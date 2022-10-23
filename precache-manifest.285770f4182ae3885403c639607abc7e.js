2967e3ddaa7d1321d3928ee0718193ce214d8f7d
lemus <lemus777@gmail.com>
2022-10-23 14:20:27 +0200

    Updates

---
+++ precache-manifest.285770f4182ae3885403c639607abc7e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ac817eee1ce08e4e61a",
+    "url": "/react-router-todo/static/css/main.9e882800.chunk.css"
+  },
+  {
+    "revision": "1ac817eee1ce08e4e61a",
+    "url": "/react-router-todo/static/js/main.1ac817ee.chunk.js"
+  },
+  {
+    "revision": "54bca721865cbcfa19fc",
+    "url": "/react-router-todo/static/js/1.54bca721.chunk.js"
+  },
+  {
+    "revision": "2cd62036b33e63994fad",
+    "url": "/react-router-todo/static/js/runtime~main.2cd62036.js"
+  },
+  {
+    "revision": "988d653ffdeac0d34a25575d53672edb",
+    "url": "/react-router-todo/static/media/check.988d653f.svg"
+  },
+  {
+    "revision": "7bb702cff6a8acd6b9ee72859ad070db",
+    "url": "/react-router-todo/static/media/edit.7bb702cf.svg"
+  },
+  {
+    "revision": "9b8d46c07ebbc7e651133c437f87a194",
+    "url": "/react-router-todo/static/media/delete.9b8d46c0.svg"
+  },
+  {
+    "revision": "361bbb86495b830c19e839bb9eb81e2f",
+    "url": "/react-router-todo/index.html"
+  }
+];
